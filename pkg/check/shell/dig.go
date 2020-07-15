05644dfad3499aa9610b1dbbc09185cde56d93d1
bakito <github@bakito.ch>
2020-07-15 15:39:25 +0200

    nandle dig answer

--- pkg/check/shell/dig.go
+++ pkg/check/shell/dig.go
@@ -2,6 +2,7 @@ package shell
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"os/exec"
 	"regexp"
@@ -12,11 +13,12 @@ import (
 )
 
 const (
-	digCommand = "dig %s +noall +stats"
+	digCommand = "dig %s"
 )
 
 var (
 	queryTimePattern = regexp.MustCompile(`.*;; Query time: (\d+) msec.*`)
+	noErrorPattern   = regexp.MustCompile(`.*status: NOERROR.*`)
 )
 
 // New create a new dig command check
@@ -42,9 +44,16 @@ func (c *digCheck) Run(ctx context.Context, address check.Address) *check.Result
 	if err != nil {
 		return res
 	}
+
+	// Mis
+	if !noErrorPattern.Match(out) {
+		res.Err = errors.New(string(out))
+		return res
+	}
+
 	log.WithField("command", "dig").Debugf("%s\n", out)
 
-	if queryTimePattern.MatchString(string(out)) {
+	if queryTimePattern.Match(out) {
 		m := queryTimePattern.FindStringSubmatch(string(out))
 		dur, _ := time.ParseDuration(fmt.Sprintf("%sms", m[1]))
 		res.Duration = &dur
